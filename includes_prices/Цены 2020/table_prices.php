a25c3c94a17b1c5f0af78db017afca2f233a601e
Александр <85952813+aleksandrburenkov@users.noreply.github.com>
2024-05-02 09:57:39 +0300

    Add files via upload

---
+++ includes_prices/Цены 2020/table_prices.php
@@ -0,0 +1,416 @@
+<?php
+
+    $table_prices = array();
+    
+    /*
+    type_material -> Тип материала
+    destination -> Место назначения
+    group -> Отдельные группы
+    visors_doors -> Козырбки для дверей
+    form -> По форме
+    carports -> Навесы для авто
+    hrill -> Для мангала
+    polycarbonate -> Из поликарбоната
+    */
+    
+    $table_prices['type_material'][] = [
+        
+            'title' => 'Навес из поликарбоната',
+            'unit' => 'м2',
+            'price' => 'от 2 900 ₽'
+        
+        ];
+    
+    $table_prices['type_material'][] = [
+        
+            'title' => 'Навес из профнастила',
+            'unit' => 'м2',
+            'price' => 'от 3 400 ₽'
+        
+        ];
+    
+    $table_prices['type_material'][] = [
+        
+            'title' => 'Навес из черепицы',
+            'unit' => 'м2',
+            'price' => 'от 3 500 ₽'
+        
+        ];
+    
+    $table_prices['type_material'][] = [
+        
+            'title' => 'Навес с мягкой кровли',
+            'unit' => 'м2',
+            'price' => 'от 4 900 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для автомобиля',
+            'unit' => 'м2',
+            'price' => 'от 2 900 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес над крыльцом',
+            'unit' => 'м2',
+            'price' => 'от 4 000 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для террасы',
+            'unit' => 'м2',
+            'price' => 'от 2 800 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для беседки',
+            'unit' => 'м2',
+            'price' => 'от 2 800 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для мангала',
+            'unit' => 'м2',
+            'price' => 'от 5 000 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для барбекю',
+            'unit' => 'м2',
+            'price' => 'от 2 800 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес над калиткой',
+            'unit' => 'м2',
+            'price' => 'от 4 000 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для бассейна',
+            'unit' => 'м2',
+            'price' => 'от 2 800 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Качели с навесом',
+            'unit' => 'м2',
+            'price' => 'от 10 000 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навесы на ворота',
+            'unit' => 'м2',
+            'price' => 'от 2 600 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес на дачу',
+            'unit' => 'м2',
+            'price' => 'от 2 800 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес к дому',
+            'unit' => 'м2',
+            'price' => 'от 2 800 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Беседки',
+            'unit' => 'шт.',
+            'price' => 'от 12 190 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Мангалы с крышей',
+            'unit' => 'шт.',
+            'price' => 'от 10 000 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Козырек над крыльцом',
+            'unit' => 'шт.',
+            'price' => 'от 5 000 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Усиленные навесы',
+            'unit' => 'шт.',
+            'price' => 'от 5 000 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Монтаж навесов',
+            'unit' => 'шт.',
+            'price' => 'от 250 ₽'
+        
+        ];
+    
+    $table_prices['visors_doors'][] = [
+        
+            'title' => 'Козырек 3х4',
+            'unit' => 'м',
+            'price' => 'от 22 400 ₽'
+        
+        ];
+    
+    $table_prices['visors_doors'][] = [
+        
+            'title' => 'Козырек 4х2',
+            'unit' => 'м',
+            'price' => 'от 21 120 ₽'
+        
+        ];
+    
+    $table_prices['visors_doors'][] = [
+        
+            'title' => 'Козырек 2х2',
+            'unit' => 'м',
+            'price' => 'от 16 200 ₽'
+        
+        ];
+    
+    $table_prices['visors_doors'][] = [
+        
+            'title' => 'Козырек 2х3',
+            'unit' => 'м',
+            'price' => 'от 33 600 ₽'
+        
+        ];
+    
+    $table_prices['form'][] = [
+        
+            'title' => 'Односкатный навес',
+            'unit' => 'м2',
+            'price' => 'от 2 600 ₽'
+        
+        ];
+    
+    $table_prices['form'][] = [
+        
+            'title' => 'Двускатный навес',
+            'unit' => 'м2',
+            'price' => 'от 2 800 ₽'
+        
+        ];
+    
+    $table_prices['form'][] = [
+        
+            'title' => 'Треугольный навес',
+            'unit' => 'м2',
+            'price' => 'от 2 800 ₽'
+        
+        ];
+    
+    $table_prices['form'][] = [
+        
+            'title' => 'Сферический навес',
+            'unit' => 'м2',
+            'price' => 'от 3 000 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 4х5,5',
+            'unit' => 'м',
+            'price' => 'от 33 700 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 3х6',
+            'unit' => 'м',
+            'price' => 'от 39 200 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 4х4',
+            'unit' => 'м',
+            'price' => 'от 44 800 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 4х5',
+            'unit' => 'м',
+            'price' => 'от 56 000 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 3х4',
+            'unit' => 'м',
+            'price' => 'от 84 000 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 3,5х4',
+            'unit' => 'м',
+            'price' => 'от 61 100 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для 2 авто 6х5',
+            'unit' => 'м',
+            'price' => 'от 50 400 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 4х6',
+            'unit' => 'м',
+            'price' => 'от 20 000 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для 2 авто 3х8,2',
+            'unit' => 'м',
+            'price' => 'от 68 880 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для 2 авто 4х8,2',
+            'unit' => 'м',
+            'price' => 'от 91 840 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для двух авто 5х8,2',
+            'unit' => 'м',
+            'price' => 'от 114 800 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для 2 авто 5,5х8,2',
+            'unit' => 'м',
+            'price' => 'от 126 280 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для мангала 2х1',
+            'unit' => 'м',
+            'price' => 'от 22 400 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для мангала 3х1',
+            'unit' => 'м',
+            'price' => 'от 39 200 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для барбекю 5х2',
+            'unit' => 'м',
+            'price' => 'от 5 600 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для барбекю 4х2',
+            'unit' => 'м',
+            'price' => 'от 8 400 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для барбекю 7х2',
+            'unit' => 'м',
+            'price' => 'от 28 000 ₽'
+        
+        ];
+    
+    $table_prices['polycarbonate'][] = [
+        
+            'title' => 'Беседка 3х3',
+            'unit' => 'м',
+            'price' => 'от 25 200 ₽'
+        
+        ];
+    
+    $table_prices['polycarbonate'][] = [
+        
+            'title' => 'Беседка 4х4',
+            'unit' => 'м',
+            'price' => 'от 44 800 ₽'
+        
+        ];
+    
+    $table_prices['polycarbonate'][] = [
+        
+            'title' => 'Беседка 5х5',
+            'unit' => 'м',
+            'price' => 'от 70 000 ₽'
+        
+        ];
+    
+    $table_prices['polycarbonate'][] = [
+        
+            'title' => 'Беседка 6х6',
+            'unit' => 'м',
+            'price' => 'от 99 800 ₽'
+        
+        ];
+
+?>
---
+++ includes_prices/Цены 2021-2022/table_prices 2022.php
@@ -0,0 +1,416 @@
+<?php
+
+    $table_prices = array();
+    
+    /*
+    type_material -> Тип материала
+    destination -> Место назначения
+    group -> Отдельные группы
+    visors_doors -> Козырбки для дверей
+    form -> По форме
+    carports -> Навесы для авто
+    hrill -> Для мангала
+    polycarbonate -> Из поликарбоната
+    */
+    
+    $table_prices['type_material'][] = [
+        
+            'title' => 'Навес из поликарбоната',
+            'unit' => 'м2',
+            'price' => 'от 4 180 ₽'
+        
+        ];
+    
+    $table_prices['type_material'][] = [
+        
+            'title' => 'Навес из профнастила',
+            'unit' => 'м2',
+            'price' => 'от 4 200 ₽'
+        
+        ];
+    
+    $table_prices['type_material'][] = [
+        
+            'title' => 'Навес из черепицы',
+            'unit' => 'м2',
+            'price' => 'от 4 400 ₽'
+        
+        ];
+    
+    $table_prices['type_material'][] = [
+        
+            'title' => 'Навес с мягкой кровли',
+            'unit' => 'м2',
+            'price' => 'от 6 200 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для автомобиля',
+            'unit' => 'м2',
+            'price' => 'от 4 200 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес над крыльцом',
+            'unit' => 'м2',
+            'price' => 'от 4 400 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для террасы',
+            'unit' => 'м2',
+            'price' => 'от 4 270 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для беседки',
+            'unit' => 'м2',
+            'price' => 'от 4 100 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для мангала',
+            'unit' => 'м2',
+            'price' => 'от 4 700 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для барбекю',
+            'unit' => 'м2',
+            'price' => 'от 4 200 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес над калиткой',
+            'unit' => 'м2',
+            'price' => 'от 4 400 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес для бассейна',
+            'unit' => 'м2',
+            'price' => 'от 4 300 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Качели с навесом',
+            'unit' => 'м2',
+            'price' => 'от 27 000 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навесы на ворота',
+            'unit' => 'м2',
+            'price' => 'от 5 800 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес на дачу',
+            'unit' => 'м2',
+            'price' => 'от 4 100 ₽'
+        
+        ];
+    
+    $table_prices['destination'][] = [
+        
+            'title' => 'Навес к дому',
+            'unit' => 'м2',
+            'price' => 'от 4 250 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Беседки',
+            'unit' => 'шт.',
+            'price' => 'от 49 190 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Мангалы с крышей',
+            'unit' => 'шт.',
+            'price' => 'от 15 000 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Козырек над крыльцом',
+            'unit' => 'шт.',
+            'price' => 'от 23 000 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Усиленные навесы',
+            'unit' => 'шт.',
+            'price' => 'от 5 750 ₽'
+        
+        ];
+    
+    $table_prices['group'][] = [
+        
+            'title' => 'Монтаж навесов',
+            'unit' => 'шт.',
+            'price' => 'от 890 ₽'
+        
+        ];
+    
+    $table_prices['visors_doors'][] = [
+        
+            'title' => 'Козырек 3х4',
+            'unit' => 'м',
+            'price' => 'от 22 400 ₽'
+        
+        ];
+    
+    $table_prices['visors_doors'][] = [
+        
+            'title' => 'Козырек 4х2',
+            'unit' => 'м',
+            'price' => 'от 21 120 ₽'
+        
+        ];
+    
+    $table_prices['visors_doors'][] = [
+        
+            'title' => 'Козырек 2х2',
+            'unit' => 'м',
+            'price' => 'от 19 200 ₽'
+        
+        ];
+    
+    $table_prices['visors_doors'][] = [
+        
+            'title' => 'Козырек 2х3',
+            'unit' => 'м',
+            'price' => 'от 23 600 ₽'
+        
+        ];
+    
+    $table_prices['form'][] = [
+        
+            'title' => 'Односкатный навес',
+            'unit' => 'м2',
+            'price' => 'от 3 950 ₽'
+        
+        ];
+    
+    $table_prices['form'][] = [
+        
+            'title' => 'Двускатный навес',
+            'unit' => 'м2',
+            'price' => 'от 4 080 ₽'
+        
+        ];
+    
+    $table_prices['form'][] = [
+        
+            'title' => '4х скатный навес',
+            'unit' => 'м2',
+            'price' => 'от 4 300 ₽'
+        
+        ];
+    
+    $table_prices['form'][] = [
+        
+            'title' => 'Сферический навес',
+            'unit' => 'м2',
+            'price' => 'от 4 140 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 4х5,5',
+            'unit' => 'м',
+            'price' => 'от 89 720 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 3х6',
+            'unit' => 'м',
+            'price' => 'от 74 605 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 4х4',
+            'unit' => 'м',
+            'price' => 'от 65 260 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 4х5',
+            'unit' => 'м',
+            'price' => 'от 82 080 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 3х4',
+            'unit' => 'м',
+            'price' => 'от 49 440 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 3,5х4',
+            'unit' => 'м',
+            'price' => 'от 57 890 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для 2 авто 6х5',
+            'unit' => 'м',
+            'price' => 'от 123 450 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для авто 4х6',
+            'unit' => 'м',
+            'price' => 'от 98 810 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для 2 авто 3х8,2',
+            'unit' => 'м',
+            'price' => 'от 99 620 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для 2 авто 4х8,2',
+            'unit' => 'м',
+            'price' => 'от 134 160 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для двух авто 5х8,2',
+            'unit' => 'м',
+            'price' => 'от 197 780 ₽'
+        
+        ];
+    
+    $table_prices['carports'][] = [
+        
+            'title' => 'Навес для 2 авто 5,5х8,2',
+            'unit' => 'м',
+            'price' => 'от 168 970 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для мангала 2х1',
+            'unit' => 'шт',
+            'price' => 'от 22 400 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для мангала 3х1',
+            'unit' => 'шт',
+            'price' => 'от 24 200 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для барбекю 5х2',
+            'unit' => 'шт',
+            'price' => 'от 43 600 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для барбекю 4х2',
+            'unit' => 'шт',
+            'price' => 'от 41 400 ₽'
+        
+        ];
+    
+    $table_prices['hrill'][] = [
+        
+            'title' => 'Для барбекю 7х2',
+            'unit' => 'шт',
+            'price' => 'от 76 140 ₽'
+        
+        ];
+    
+    $table_prices['polycarbonate'][] = [
+        
+            'title' => 'Беседка 3х3',
+            'unit' => 'м',
+            'price' => 'от 49 800 ₽'
+        
+        ];
+    
+    $table_prices['polycarbonate'][] = [
+        
+            'title' => 'Беседка 4х4',
+            'unit' => 'м',
+            'price' => 'от 58 840 ₽'
+        
+        ];
+    
+    $table_prices['polycarbonate'][] = [
+        
+            'title' => 'Беседка 5х5',
+            'unit' => 'м',
+            'price' => 'от 87 230 ₽'
+        
+        ];
+    
+    $table_prices['polycarbonate'][] = [
+        
+            'title' => 'Беседка 6х6',
+            'unit' => 'м',
+            'price' => 'от 123 340 ₽'
+        
+        ];
+
+?>
